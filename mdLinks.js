7dec12a28058c79d34f8f5631b562aa6407be0b8
Dea Gatell <deagatell6@gmail.com>
2023-06-20 12:49:14 -0500

    Formato de mdLinks

--- mdLinks.js
+++ mdLinks.js
@@ -1,14 +1,22 @@
-const { isAbsoluteR, isRelative, isValid, isFileOrDirectory, isMarkdownFile } = require('./index.js');
+const {
+  isAbsoluteR,
+  isRelative,
+  isValid,
+  isFileOrDirectory,
+  isMarkdownFile,
+} = require("./index.js");
 
 function mdLinks(route) {
-   return {
-      isAbsoluteR: isAbsoluteR(route),
-      isRelative: isRelative(route),
-      isValid: isValid(route),
-      isFileOrDirectory: isFileOrDirectory(route),
-      isMarkdownFile: isMarkdownFile(route)
-   };
+  return {
+    isAbsoluteR: isAbsoluteR(route),
+    isRelative: isRelative(route),
+    isValid: isValid(route),
+    isFileOrDirectory: isFileOrDirectory(route),
+    isMarkdownFile: isMarkdownFile(route),
+  };
 }
 
-console.log(mdLinks('C:/Users/USER/Desktop/Proyecto4/DEV006-md-links/README.md'));
+console.log(
+  mdLinks("C:/Users/USER/Desktop/Proyecto4/DEV006-md-links/README.md")
+);
 //Retorna promesa
