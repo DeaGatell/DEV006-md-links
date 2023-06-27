35a6c42ca76363d053171213eaff3e04edd7a40a
Dea Gatell <deagatell6@gmail.com>
2023-06-27 12:26:13 -0500

    Planificación mdLinks

--- mdLinks.js
+++ mdLinks.js
@@ -7,7 +7,7 @@ const {
 } = require("./index.js");
 
 // Función que devuelve un objeto con los resultados de las funciones de verificación de la ruta
-function mdLinks(route) {
+function functionConfirmation(route) {
   return {
     isAbsoluteR: isAbsoluteR(route),
     isRelative: isRelative(route),
@@ -18,8 +18,17 @@ function mdLinks(route) {
 }
 
 console.log(
-  mdLinks("C:/Users/USER/Desktop/Proyecto4/DEV006-md-links/README.md")
+  functionConfirmation("C:/Users/USER/Desktop/Proyecto4/DEV006-md-links/README.md")
 );
+
+function mdLinks(path, options) {
+  return new Promise((resolve, reject) => {
+    // Verificar y procesar la ruta proporcionada
+    // Obtener la lista de archivos o el contenido del archivo Markdown
+    // Extraer los enlaces y realizar validaciones si es necesario
+    // Devolver los resultados como un arreglo de objetos
+  });
+}
 
 // Ejemplo de uso de la función isMarkdownFile con una ruta específica
 //const filePath = 'C://Users//USER//Desktop//Proyecto4//DEV006-md-links//contents//prueba.md';
