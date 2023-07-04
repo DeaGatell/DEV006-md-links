fe04bb9c95813d6df41ce639a7f030e566bfd91e
Dea Gatell <deagatell6@gmail.com>
2023-07-04 18:41:02 -0500

    Test de mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,18 @@
-const mdLinks = require('../');
+const { mdLinks } = require("../mdLinks.js");
+const pathUser = "./mock-directory/mockREADME2.md";
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("mdLinks", () => {
+  it("Deberia retornar una promesa que se resuelve con un array de objetos", (done) => {
+    const result = mdLinks(pathUser, options = { validate: true });
+    expect(result)
+      .resolves.toEqual([
+        {
+          href: 'https://nodejs.org/es/about/',
+          text: 'Acerca de Node.js - Documentación oficial',
+          file: 'C:\\Users\\USER\\Desktop\\Proyecto4\\DEV006-md-links\\mock-directory\\mockREADME2.md',
+          status: 200,
+          statusText: 'OK'
+        }
+      ]).then(done);
   });
-
 });
